19de4b20845e7188af9e82be24c9502451bfb0d6
Seth Shikora <seth@shikora.org>
2019-10-20 20:55:04 -0700

    add tests

--- index.js
+++ index.js
@@ -2,46 +2,43 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
- // For more information on building apps:
- // https://probot.github.io/docs/
- // const { createRobot } = require('./robot')
+// For more information on building apps:
+// https://probot.github.io/docs/
+// const { createRobot } = require('./robot')
 
- // To get your app running against GitHub, see:
- // https://probot.github.io/docs/development/
+// To get your app running against GitHub, see:
+// https://probot.github.io/docs/development/
 
- const get = require('lodash.get')
+const get = require('lodash.get')
 
- module.exports = app => {
-   app.log('Started SemaphoreCI Reporter tool')
+module.exports = app => {
+  app.log('Started SemaphoreCI Reporter tool')
 
-   app.on('status', async context => {
-     const eventContext = get(context, 'payload.context')
-     if (eventContext.indexOf('semaphoreci') <= 0) { return }
-     context.log('Found SemaphoreCI status event')
-     const state = get(context, 'payload.state')
-     if (state === 'pending') { return }
-     const targetUrl = get(context, 'payload.target_url')
+  app.on('status', async context => {
+    const eventContext = get(context, 'payload.context')
+    if (eventContext.indexOf('semaphoreci') <= 0) { return }
+    context.log('Found SemaphoreCI status event')
+    const state = get(context, 'payload.state')
+    if (state === 'pending') { return }
+    const targetUrl = get(context, 'payload.target_url')
 
-     const body =
+    const body =
        state === 'success'
          ? `Your build is green! Your good to go! [green build](${targetUrl})!`
          : `The Pull Request failed. The build is here [build](${targetUrl})`
-     const { owner, repo } = context.repo()
-     const commit_id = get(context, 'payload.commit.sha')
+    const { owner, repo } = context.repo()
+    // const commit_id = get(context, 'payload.commit.sha')
 
-     const branchName = get(context, 'payload.branches[0].name')
+    const branchName = get(context, 'payload.branches[0].name')
 
-     const allPullRequests = await context.github.pullRequests.getAll({ owner, repo })
+    const allPullRequests = await context.github.pullRequests.getAll({ owner, repo })
 
-     const pullRequest = get(allPullRequests, 'data', [])
-       .filter(pr => get(pr, 'head.ref') === branchName)
-       [0]
+    const pullRequest = get(allPullRequests, 'data', [])
+      .filter(pr => get(pr, 'head.ref') === branchName)[0]
 
-     if (!pullRequest) return
-     const { number } = pullRequest  // Pull request and issue number are identical!
+    if (!pullRequest) return
+    const { number } = pullRequest // Pull request and issue number are identical!
 
-     await context.github.issues.createComment({ owner, repo, number, body })
-
-   })
-
+    await context.github.issues.createComment({ owner, repo, number, body })
+  })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,26 @@
 const nock = require('nock')
 // Requiring our app implementation
-const myProbotApp = require('..')
+const ciReporter = require('..')
 const { Probot } = require('probot')
 // Requiring our fixtures
-const payload = require('./fixtures/issues.opened')
+const payload = require('./fixtures/pull_request.opened')
+const payloadSuccess = require('./fixtures/pull_request.opened-success')
+const compare = require('./fixtures/compare')
+const compareSuccess = require('./fixtures/compare-success')
+const checkRun = require('./fixtures/check_run.created')
+const checkRunSuccess = require('./fixtures/check_run.created-success')
+
 const issueCreatedBody = { body: 'Thanks for opening this issue!' }
 
 nock.disableNetConnect()
 
-describe('My Probot app', () => {
+describe('CI Reporter', () => {
   let probot
 
   beforeEach(() => {
     probot = new Probot({})
     // Load our app into probot
-    const app = probot.load(myProbotApp)
+    const app = probot.load(ciReporter)
 
     // just return a test token
     app.app = () => 'test'
@@ -36,6 +42,54 @@ describe('My Probot app', () => {
 
     // Receive a webhook event
     await probot.receive({ name: 'issues', payload })
+  })
+
+  test('creates a failing check', async () => {
+    nock('https://api.github.com')
+      .post('/app/installations/13055/access_tokens')
+      .reply(200, { token: 'test' })
+
+    nock('https://api.github.com')
+      .get('/repos/robotland/test/contents/.github/ci-reporter.yml')
+      .reply(404)
+
+    nock('https://api.github.com')
+      .get('/repos/robotland/test/compare/607c64cd8e37eb2db939f99a17bee5c7d1a90a31...e76ed6025cec8879c75454a6efd6081d46de4c94')
+      .reply(200, compare)
+
+    nock('https://api.github.com')
+      .post('/repos/robotland/test/check-runs', (body) => {
+        body.completed_at = '2018-07-14T18:18:54.156Z'
+        expect(body).toMatchObject(checkRun)
+        return true
+      })
+      .reply(200)
+
+    await probot.receive({ name: 'pull_request', payload })
+  })
+
+  test('creates a passing check', async () => {
+    nock('https://api.github.com')
+      .post('/app/installations/13055/access_tokens')
+      .reply(200, { token: 'test' })
+
+    nock('https://api.github.com')
+      .get('/repos/octocat/Hello-World/contents/.github/ci-reporter.yml')
+      .reply(404)
+
+    nock('https://api.github.com')
+      .get('/repos/octocat/Hello-World/compare/a10867b14bb761a232cd80139fbd4c0d33264240...34c5c7793cb3b279e22454cb6750c80560547b3a')
+      .reply(200, compareSuccess)
+
+    nock('https://api.github.com')
+      .post('/repos/octocat/Hello-World/check-runs', (body) => {
+        body.completed_at = '2018-07-14T18:18:54.156Z'
+        expect(body).toMatchObject(checkRunSuccess)
+        return true
+      })
+      .reply(200)
+
+    await probot.receive({ name: 'pull_request', payload: payloadSuccess })
   })
 })
 
